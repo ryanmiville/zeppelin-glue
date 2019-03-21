82ffb7820eaf562ade5abcfd951824f9414f9b55
Ryan Miville <ryan.miville@terminus.com>
2019-03-21 10:33:49 -0400

    check status

--- start.sh
+++ start.sh
@@ -5,6 +5,18 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+echo "waiting for status to be READY"
+
+status=`aws glue get-dev-endpoint --endpoint-name $1 --query 'DevEndpoint.Status' --output text`
+echo "status is $status"
+
+until [ "$status" == "READY" ]
+do
+	status=`aws glue get-dev-endpoint --endpoint-name $1 --query 'DevEndpoint.Status' --output text`
+	sleep 15
+	echo "status is $status"
+done
+
 name=`aws glue get-dev-endpoint --endpoint-name $1 --query 'DevEndpoint.PublicAddress' --output text`
 
 ENDPOINT=$name docker-compose up --build
